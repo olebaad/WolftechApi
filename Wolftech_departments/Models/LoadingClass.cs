5a8e88b0510a81e3d60b68360861d14b86ae32bc
Ole Baadshaug <olebaadshaug@gmail.com>
2020-06-05 11:43:39 +0200

    feat(LoadingClass.cs): adding model class for loading data from disk

---
+++ Wolftech_departments/Models/LoadingClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WolftechApi.Models
+{
+    public class LoadingClass
+    {
+        public int OID { get; set; }
+        public string Title { get; set; }
+        public string Color { get; set; }
+        public int? DepartmentParent_OID { get; set; }
+    }
+}
