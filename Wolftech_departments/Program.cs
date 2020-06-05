affef2eae85f9ced0185353e594256e1feefbd55
Ole Baadshaug <olebaadshaug@gmail.com>
2020-06-05 11:38:46 +0200

    fix: changing namespace name

--- Wolftech_departments/Program.cs
+++ Wolftech_departments/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Wolftech_departments
+namespace WolftechApi
 {
     public class Program
     {
